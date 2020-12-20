2e89d4ed8b1fafac6c6f0fb23585a3e2f7d10e71
Alexandre <carronalexandre77@gmail.com>
2020-12-20 19:43:35 +0100

    cration class

---
+++ js/liste_produits.js
@@ -0,0 +1,10 @@
+class Ours {
+    constructor (name, price, description, imageURL){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageURL = imageURL;
+    }
+}
+
+let ours1 = new Ours('teddy1', 45, true, /Users/alexandre/Documents/GitHub/AlexandreCarron_5_13112020/images/teddy_2.jpg;
