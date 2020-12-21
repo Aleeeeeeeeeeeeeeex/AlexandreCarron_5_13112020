aacd80b1ff8d05edfb86135af3e1b7803a8b7547
Alexandre <carronalexandre77@gmail.com>
2020-12-21 11:35:27 +0100

    js function boucle

--- js/liste_produits.js
+++ js/liste_produits.js
@@ -7,4 +7,24 @@ class Ours {
     }
 }
 
-let ours1 = new Ours('teddy1', 45, true, /Users/alexandre/Documents/GitHub/AlexandreCarron_5_13112020/images/teddy_2.jpg;
+let ours1 = new Ours('teddy1', 45, true, 'https://alex2020git.github.io/AlexandreCarron_5_13112020/images/teddy_2.jpg');
+let ours2 = new Ours('teddy2', 45, true, 'https://alex2020git.github.io/AlexandreCarron_5_13112020/images/teddy_2.jpg');
+let ours3 = new Ours('teddy3', 45, true, 'https://alex2020git.github.io/AlexandreCarron_5_13112020/images/teddy_2.jpg');
+let ours4 = new Ours('teddy4', 45, true, 'https://alex2020git.github.io/AlexandreCarron_5_13112020/images/teddy_2.jpg');
+
+
+let listeOurs = [];
+episodes.push(ours1, ours2, ours3, ours4);
+
+const listeVue = document.getElementById('liste');
+
+for (let i in listeOurs) {
+    let productOurs = document.createElement('div');
+    productOurs.classList.add('ours1');
+    let imageOurs = document.createElement('div');
+    imageOurs.classList.add('ours1_img');
+    productOurs.append(imageOurs);
+
+};
+
+
